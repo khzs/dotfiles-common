cbd26a22ddafe539e1e67a062d3fb88ef356f9da
khzs <zsolt.kocsi-horvath@autodesk.com>
2024-06-24 10:18:22 +0200

    workmac specific

--- zshrc-common-rice.sh
+++ zshrc-common-rice.sh
@@ -2,7 +2,7 @@ export HOMEBREW_NO_ENV_HINTS=1
 
 count()   { git rev-list --count HEAD ^$@                           }
 
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 bindkey '\t\t' autosuggest-accept
 
 # this should be at the very very end of .zshrc
